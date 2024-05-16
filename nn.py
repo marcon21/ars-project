1cd95cc631264f484a0ebfdd6e699ba6ae41ea3c
tarcisio16 <160622023+tarcisio16@users.noreply.github.com>
2024-05-16 18:11:19 +0200

    comments

--- nn.py
+++ nn.py
@@ -18,8 +18,8 @@ class NN(nn.Module):
     '''
     def __init__(self, n_sensors=12, x1=32, x2=4, activation=F.relu):
         super(NN, self).__init__()
-        #self.fc1 = nn.Linear(n_sensors+x2, x1)
-        self.fc1 = nn.Linear(n_sensors, x1)
+        #self.fc1 = nn.Linear(n_sensors+x2, x1)  TOADD
+        self.fc1 = nn.Linear(n_sensors, x1)  #TOREMOVE
         self.fc2 = nn.Linear(x1, x2)
         self.fc3 = nn.Linear(x2, 2)
         self.activation = activation
@@ -30,12 +30,12 @@ class NN(nn.Module):
         '''
         if not isinstance(x, torch.Tensor):
             raise ValueError("Input should be a torch.Tensor")
-        self.fc1.weight.data = torch.randn_like(self.fc1.weight.data)
-        self.fc2.weight.data = torch.randn_like(self.fc2.weight.data)
-        self.fc3.weight.data = torch.randn_like(self.fc3.weight.data)
-        self.fc1.bias.data = torch.randn_like(self.fc1.bias.data)  
-        self.fc2.bias.data = torch.randn_like(self.fc2.bias.data)
-        self.fc3.bias.data = torch.randn_like(self.fc3.bias.data)
+        self.fc1.weight.data = torch.randn_like(self.fc1.weight.data) #TOREMOVE
+        self.fc2.weight.data = torch.randn_like(self.fc2.weight.data) #TOREMOVE
+        self.fc3.weight.data = torch.randn_like(self.fc3.weight.data) #TOREMOVE
+        self.fc1.bias.data = torch.randn_like(self.fc1.bias.data)  #TOREMOVE
+        self.fc2.bias.data = torch.randn_like(self.fc2.bias.data) #TOREMOVE
+        self.fc3.bias.data = torch.randn_like(self.fc3.bias.data) #TOREMOVE
         x = self.activation(self.fc1(x))
         x = self.activation(self.fc2(x))
         vr = x[0].item()
