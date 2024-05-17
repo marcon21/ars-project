1a1c818c6f238d25ab52080555a24b07be35e060
tarcisio16 <160622023+tarcisio16@users.noreply.github.com>
2024-05-17 23:36:30 +0200

    not finished, but added the evolution part

--- main.py
+++ main.py
@@ -2,21 +2,63 @@ import pygame
 from pygame.locals import *
 from actors import Agent, Wall, Landmark
 from evolved_agent import EvolvedAgent
-from env_evolution import PygameEvolvedEnviroment
+from env_evolution import PygameEvolvedEnviroment, EnvEvolution
 from utils import intersection, distance_from_wall, angle_from_vector
 from parameters import *
 from nn import NN
+from evolution import Evolution
+
+
+'''
+    1. The evolution of the agents
+    
+
+    The evolution of the agents is done in the following way:
+    
+    1.1. Create the population of agents
+    1.2. Create the enviroments
+    1.3. Run the simulation for a number of generations
+    1.4. For each generation:
+        1.4.1. Load the landmarks and the walls in the enviroments
+        1.4.2. Run the simulation for a number of instants
+        1.4.3. Compute the fitness score of each agent
+        1.5. Selection, mutation and crossover of the agents
+        1.6. Update the population of agents
+        
+'''
+#create population of agents
+fitness_scores = np.zeros(AGENT_NUMBER)
+agents = [EvolvedAgent(x=X_START,y = Y_START,n_sensors=N_SENSORS, controller=NN(N_SENSORS,activation=ACTIVATION),size=AGENT_SIZE,color=AGENT_COLOR, max_distance=MAX_DISTANCE) for _ in range(AGENT_NUMBER)]
+envs = [EnvEvolution(agent, height=HEIGHT, width=WIDTH, instants=INSTANTS, w1=W1, w2=W2, w3=W3) for agent in agents]
+
+#evolution
+evl = Evolution(initial_population=agents) # io direi di passare alcuni parametri in input, che gestiscono l'evoluzione come la procedura di selezione, crossover e mutazione
+for generation in range(GENERATIONS):
+    for env in envs:
+        env.load_landmarks(LANDMARK_TXT, LANDMARK_SIZE, LANDMARK_COLOR)
+        env.load_walls(WALLS_TXT)
+        for _ in range(INSTANTS):
+            env.move_agent()
+        fitness_scores[envs.index(env)] = env.fitness_score()
+    print(f"Generation {generation} - Fitness scores: {fitness_scores}")
+    
+    #selection, mutation and crossover, update the population
+    evl.selection(fitness_scores)
+    evl.mutation()
+    evl.crossover()
+        
+best_agents = evl.best_agent()
+
+'''
+    2. Show the best agents    
+'''
 
 pygame.init()
-
 windows = [pygame.display.set_mode(GAME_RES, HWACCEL | HWSURFACE | DOUBLEBUF) for _ in range(AGENT_NUMBER)]
-agents = [EvolvedAgent(x=X_START,y = Y_START,n_sensors=12, controller=NN(),size=AGENT_SIZE,color=AGENT_COLOR, max_distance=MAX_RANGE) for _ in range(AGENT_NUMBER)]
-envs = [PygameEvolvedEnviroment(agent, height=HEIGHT, width=WIDTH, instants=INSTANTS, w1=W1, w2=W2, w3=W3) for agent in agents]
+envs = [PygameEvolvedEnviroment(agent, height=HEIGHT, width=WIDTH, instants=INSTANTS, w1=W1, w2=W2, w3=W3) for agent in best_agents]
 for env in envs:
     env.load_landmarks(LANDMARK_TXT, LANDMARK_SIZE, LANDMARK_COLOR)
     env.load_walls(WALLS_TXT)
-    
-    
 clock = pygame.time.Clock()
 pygame.display.set_caption(GAME_TITLE)
 frame_count = 0
