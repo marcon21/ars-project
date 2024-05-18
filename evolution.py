ca644eff22cf318518ea3f1b9f6973d170527ac3
tarcisio16 <160622023+tarcisio16@users.noreply.github.com>
2024-05-18 23:14:00 +0200

    evolution first implementation

--- evolution.py
+++ evolution.py
@@ -1,6 +1,7 @@
 from env import Enviroment
 from actors import Agent
 from nn import NN
+from evolved_agent import EvolvedAgent
 from utils import create_pairs
 import math
 import numpy as np
@@ -12,21 +13,99 @@ class Evolution:
 
     def __init__(
         self,
-        env: Enviroment,
         input_dim,
         hidden_dim,
         layer_dim,
         output_dim,
-        initial_population_size,
+        initial_population,
     ):
 
-        self.env = env
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.layer_dim = layer_dim
         self.output_dim = output_dim
-        self.initial_population_size = initial_population_size
+        self.population = initial_population
+        
+    def proportionate_selection(
+        self, fitness_scores
+    ):  # Selection of the agents based on their fitness score with the proportionate selection method
 
+        sum_fitness = sum(fitness_scores)
+        probabilities = [score / sum_fitness for score in fitness_scores]
+        self.population = np.random.multinomial(
+            len(self.population), probabilities, size=None
+        )  # multinomial distribution where each agent is selected with a probability proportional to its fitness score
+
+        
+        
+    def rank_based_selection(self, fitness_scores):
+        """
+        Selection of the agents based on their fitness score with the rank-based selection method.
+        """
+        # Combine agents with their fitness scores and sort them
+        populations_scores = list(zip(self.population, fitness_scores))
+        sorted_population = sorted(populations_scores, key=lambda x: x[1], reverse=True)
+        
+        # Extract the sorted population and fitness scores
+        sorted_population, sorted_fitness_scores = zip(*sorted_population)
+        
+        # Calculate rank-based probabilities
+        ranks = np.arange(1, len(sorted_population) + 1)
+        probabilities = 1 / ranks
+        probabilities /= np.sum(probabilities)
+        
+        # Perform selection based on the calculated probabilities
+        selected_indices = np.random.choice(len(sorted_population), size=len(self.population), p=probabilities, replace=True)
+        self.population = [sorted_population[i] for i in selected_indices]
+        
+    def mutation(self, mutation_rate=0.05, mean=0, scale=0.5):
+        for agent in self.population:
+            for gene in agent.genome:
+                if np.random.rand() < mutation_rate:
+                    agent.gene = np.random.normal(loc=mean, scale=scale)
+                    
+    def crossover(self):
+
+        pairs = create_pairs(self.population, fertility_rate=0.5)
+        new_population = []
+        for pair in pairs:
+            parent1, parent2 = pair
+            child1 = Agent(
+                x=parent1.pos[0],
+                y=parent1.pos[1],
+                move_speed=5,
+                size=40,
+                n_sensors=10,
+                max_distance=200,
+                color="red",
+            )
+            child2 = Agent(
+                x=parent2.pos[0],
+                y=parent2.pos[1],
+                move_speed=5,
+                size=40,
+                n_sensors=10,
+                max_distance=200,
+                color="red",
+            )
+            for i, (p1, p2, c1, c2) in enumerate(  # Crossover between the two parents
+                zip(
+                    parent1.genetic_representation,
+                    parent2.genetic_representation,
+                    child1.genetic_representation,
+                    child2.genetic_representation,
+                )
+            ):
+                alpha = np.random.rand()  # Random number between 0 and 1
+                c1 = (
+                    alpha * p1 + (1 - alpha) * p2
+                )  # Linear combination of the two parents' genetic representations
+                c2 = alpha * p2 + (1 - alpha) * p1
+            child1.genetic_representation = c1
+            child2.genetic_representation = c2
+            new_population.append(child1)
+            new_population.append(child2)
+        self.population = new_population
     def create_model(self):
 
         return NN(self.input_dim, self.hidden_dim, self.output_dim)
@@ -85,80 +164,11 @@ class Evolution:
             )
             self.population.append(agent)
 
-    def proportionate_selection(
-        self,
-    ):  # Selection of the agents based on their fitness score with the proportionate selection method
 
-        fitness_scores = [agent.fitness_score for agent in self.population]
-        sum_fitness = sum(fitness_scores)
-        probabilities = [score / sum_fitness for score in fitness_scores]
-        self.population = np.random.multinomial(
-            len(self.population), probabilities, size=None
-        )  # multinomial distribution where each agent is selected with a probability proportional to its fitness score
 
-    def rank_based_selection(
-        self,
-    ):  # Selection of the agents based on their fitness score with the rank based selection method
 
-        sorted_population = sorted(
-            self.population, key=lambda x: x.fitness_score, reverse=True
-        )
-        probabilities = [1 / (i + 1) for i in range(len(sorted_population))]
-        probabilities /= np.sum(probabilities)
-        self.population = np.random.multinomial(
-            len(self.population), probabilities, size=None
-        )
 
-    def crossover(self):
 
-        pairs = create_pairs(self.population, fertility_rate=0.5)
-        new_population = []
-        for pair in pairs:
-            parent1, parent2 = pair
-            child1 = Agent(
-                x=parent1.pos[0],
-                y=parent1.pos[1],
-                move_speed=5,
-                size=40,
-                n_sensors=10,
-                max_distance=200,
-                color="red",
-            )
-            child2 = Agent(
-                x=parent2.pos[0],
-                y=parent2.pos[1],
-                move_speed=5,
-                size=40,
-                n_sensors=10,
-                max_distance=200,
-                color="red",
-            )
-            for i, (p1, p2, c1, c2) in enumerate(  # Crossover between the two parents
-                zip(
-                    parent1.genetic_representation,
-                    parent2.genetic_representation,
-                    child1.genetic_representation,
-                    child2.genetic_representation,
-                )
-            ):
-                alpha = np.random.rand()  # Random number between 0 and 1
-                c1 = (
-                    alpha * p1 + (1 - alpha) * p2
-                )  # Linear combination of the two parents' genetic representations
-                c2 = alpha * p2 + (1 - alpha) * p1
-            child1.genetic_representation = c1
-            child2.genetic_representation = c2
-            new_population.append(child1)
-            new_population.append(child2)
-        self.population = new_population
-
-    def mutation(self, mutation_rate=0.05, mean=0, scale=0.5):
-        for agent in self.population:
-            for i, p in enumerate(agent.genetic_representation):
-                if np.random.rand() < mutation_rate:
-                    agent.genetic_representation[i] = np.random.normal(
-                        loc=mean, scale=scale
-                    )
 
     def train(
         self, n_generations
